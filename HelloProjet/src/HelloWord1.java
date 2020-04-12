c7784dd85c04b953be4053329aaa22f15b5366f7
yezhen <563739385@qq.com>
2020-04-12 23:11:04 +0800

    修改1.txt和HelloWord.txt

--- HelloProjet/src/HelloWord1.java
+++ HelloProjet/src/HelloWord1.java
@@ -5,8 +5,11 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld1!");
+		System.out.println("HelloWorld2l!");
 		System.out.println("HelloWorld2!");
 
 		System.out.println("HelloWorld3!");
+		System.out.println("HelloWorld4l!");
+		System.out.println("HelloWorld5l!");
     }
 }
