1485d3243ef5b0512f173026f11447ac12cbc53b
yezhen <563739385@qq.com>
2023-04-01 22:00:58 +0800

    解决冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,10 +1,11 @@
 import org.junit.Test;
-import org.junit.Test1;
+
 public class Main {
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!1");
+        System.out.println("HelloWorld!11");
     }
 }
