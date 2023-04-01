93afc9388ac42f5c9328da190f95dc22149d08d6
yezhen <563739385@qq.com>
2023-04-01 17:08:13 +0800

    第9次提交

--- HelloProjet/src/HelloWord1.java
+++ HelloProjet/src/HelloWord1.java
@@ -5,15 +5,16 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld1!");
-		System.out.println("HelloWorld2l!");
-		System.out.println("HelloWorld2!");
-	System.out.println("HelloWorld3r!");
-	System.out.println("HelloWorld4r!");
+        System.out.println("HelloWorld2l!");
+        System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld3r!");
+        System.out.println("HelloWorld4r!");
 
-		System.out.println("HelloWorld3!");
-		System.out.println("HelloWorld4l!");
-		System.out.println("HelloWorld5r!");
-		System.out.println("HelloWorld6l!");
-System.out.println("777777777777777!");
+        System.out.println("HelloWorld3!");
+        System.out.println("HelloWorld4l!");
+        System.out.println("HelloWorld5r!");
+        System.out.println("HelloWorld6l!");
+        System.out.println("777777777777777!");
+        System.out.println("第9次提交");
     }
 }
