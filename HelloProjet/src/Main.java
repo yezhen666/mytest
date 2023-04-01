81c59354c3748a4573cb81506ba6f02c325e23ac
yezhen666 <56527420+yezhen666@users.noreply.github.com>
2023-04-01 21:59:08 +0800

    远程1

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,10 @@
 import org.junit.Test;
-
+import org.junit.Test1;
 public class Main {
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!1");
     }
 }
