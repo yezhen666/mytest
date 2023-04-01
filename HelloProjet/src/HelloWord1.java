77b595ad8e0fe65e04e5b74d65aa4f50bcabc6ee
yezhen <563739385@qq.com>
2023-04-01 17:09:54 +0800

    Revert "第9次提交"
    This reverts commit 93afc938

--- HelloProjet/src/HelloWord1.java
+++ HelloProjet/src/HelloWord1.java
@@ -5,16 +5,15 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld1!");
-        System.out.println("HelloWorld2l!");
-        System.out.println("HelloWorld2!");
-        System.out.println("HelloWorld3r!");
-        System.out.println("HelloWorld4r!");
+		System.out.println("HelloWorld2l!");
+		System.out.println("HelloWorld2!");
+	System.out.println("HelloWorld3r!");
+	System.out.println("HelloWorld4r!");
 
-        System.out.println("HelloWorld3!");
-        System.out.println("HelloWorld4l!");
-        System.out.println("HelloWorld5r!");
-        System.out.println("HelloWorld6l!");
-        System.out.println("777777777777777!");
-        System.out.println("第9次提交");
+		System.out.println("HelloWorld3!");
+		System.out.println("HelloWorld4l!");
+		System.out.println("HelloWorld5r!");
+		System.out.println("HelloWorld6l!");
+System.out.println("777777777777777!");
     }
 }
